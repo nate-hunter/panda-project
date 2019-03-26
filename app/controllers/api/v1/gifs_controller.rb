fce0ffeaee00c011e8e9902acc49b963e750e9c4
Nate <github email address>
2019-03-26 17:30:38 -0400

    GifsController update action built

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -10,6 +10,22 @@ module Api
         panda = Gif.find(params[:id])
         render json: {status: 'SUCCESS', message: 'Loaded GIF', data: panda}, status: :ok
       end
+
+      def update
+        panda = Gif.find(params[:id])
+        if panda.update(panda_params)
+          render json: panda
+        else
+          render json: {status: 'ERROR', message: 'Panda not updated', data: panda.errors}, status: :unproccessable_entity
+        end
+      end
+
+      private
+
+      def panda_params
+        params.permit(:name, :img_url, :music, :likes)
+      end
+
     end
   end
 end
